21bec2926cd5db0842ae2b97e19413b135e79960
PeterBraza <66847642+PeterBraza@users.noreply.github.com>
2022-01-08 13:30:34 -0700

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,19 @@
+filef = open("prime.txt", "a")
+with open("prime.txt", "r") as f:
+    score = f.read()
+    score_ints = [ int(x) for x in score.split() ]
+
+size = len(score_ints)
+large = score_ints[size-1]
+counter = 0
+for i in range(large, (large * large)):
+    counter = 0
+    j = 0
+    while(score_ints[j] < int(i ** .5)):
+        if(i%score_ints[j] == 0 and (int(score_ints[j]**.5))):
+            counter = counter + 1
+        j = j + 1
+
+    if(counter == 0):
+        filef.write(str(i) + " ")
+        print(str(i))
